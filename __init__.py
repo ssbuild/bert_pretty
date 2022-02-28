fe6a15e3ffd25177e4407371569abf4e9898bcf3
王亚伟 <wangyawei@dhcc.com.cn>
2022-02-28 16:43:47 +0800

    2022年2月28日16:43:49

--- __init__.py
+++ __init__.py
@@ -1,5 +1,2 @@
 # -*- coding: utf-8 -*-
-from bert_text_pretty import tokenization
-from bert_text_pretty.cls import utils as cls
-from bert_text_pretty.ner import utils as ner
-from bert_text_pretty.relation import utils as relation
+from bert_pretty import tokenization
